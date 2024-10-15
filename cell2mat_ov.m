7bc282c20137955b5b0947e971bdb2cb9cbf8b5b
jackieb1 <54858424+jackieb1@users.noreply.github.com>
2024-10-15 10:18:18 -0400

    update

--- cell2mat_ov.m
+++ cell2mat_ov.m
@@ -11,17 +11,73 @@ function X = cell2mat_ov(I,xx_s,xx_f,yy_s,yy_f,zz_s,zz_f,overlap,sz)
 % OUTPUT:
 % X:            output matrix
 
-% Written by Eftychios A. Pnevmatikakis, Simons Foundation, 2016
+% Old version Written by Eftychios A. Pnevmatikakis, Simons Foundation, 2016
+% New version (much faster) written by Will Cunningham Economo Lab 2024
 
-X = NaN([sz,size(I{1,1},length(sz)+1)]);
-if length(sz) == 2; sz(3) = 1; end
-
-for i = 1:length(xx_f)
-    for j = 1:length(yy_f)
-        for k = 1:length(zz_f)
-            extended_grid = [max(xx_s(i)-overlap(1),1),min(xx_f(i)+overlap(1),sz(1)),max(yy_s(j)-overlap(2),1),min(yy_f(j)+overlap(2),sz(2)),max(zz_s(k)-overlap(3),1),min(zz_f(k)+overlap(3),sz(3))];
-            X(xx_s(i):xx_f(i),yy_s(j):yy_f(j),zz_s(k):zz_f(k)) = ... 
-                I{i,j,k}(1+(xx_s(i)-extended_grid(1)):end-(extended_grid(2)-xx_f(i)),1+(yy_s(j)-extended_grid(3)):end-(extended_grid(4)-yy_f(j)),1+(zz_s(k)-extended_grid(5)):end-(extended_grid(6)-zz_f(k)));
-        end
-    end
+if isa(I{1},'gpuArray')
+    dType = 'gpuArray';
+else
+    dType = 'single';
 end
+patchSize = size(I{round(size(I,1)/2),round(size(I,2)/2)});
+sameSize = cellfun(@(X) all(size(X)==patchSize),I);
+nonBorderCols = any(sameSize~=0,1);
+nonBorderRows = any(sameSize~=0,2);
+temp = cat(3,I{nonBorderRows,nonBorderCols});
+temp = reshape(temp,patchSize(1),patchSize(2),sum(nonBorderRows),sum(nonBorderCols));
+temp = permute(temp,[1,3,2,4]);
+temp = reshape(temp,patchSize(1)*sum(nonBorderRows),patchSize(2)*sum(nonBorderCols));
+
+idxBorderRows = find(~nonBorderRows);
+preBorderRows = idxBorderRows(idxBorderRows<find(nonBorderRows,1));
+postBorderRows = idxBorderRows(idxBorderRows>find(nonBorderRows,1));
+for i = 1:sum(preBorderRows>0)
+    temp = cat(1,cat(2,I{preBorderRows(i),nonBorderCols}),temp);
+end
+for i = 1:numel(postBorderRows)
+    temp = cat(1,temp,cat(2,I{postBorderRows(i),nonBorderCols}));
+end
+idxBadCols = find(~nonBorderCols);
+preBadCols = idxBadCols(idxBadCols<find(nonBorderCols,1));
+postBadCols = idxBadCols(idxBadCols>find(nonBorderCols==0,1));
+for i = 1:numel(preBadCols)
+    temp = cat(2,cat(1,I{:,preBadCols(i)}),temp);
+end
+for i = 1:numel(postBadCols)
+    temp = cat(2,temp,cat(1,I{:,postBadCols(i)}));
+end
+% figure;imagesc(temp);axis image;colormap gray;
+
+% Initialize the row transformation matrix
+A = zeros(sz(1),size(temp,1),dType);
+
+toAdd = (eye(patchSize(1),dType).*[1:overlap(1) (overlap(1)*ones(1,patchSize(1)-2*overlap(1),dType)) overlap(1):-1:1]);
+for i = 1:ceil(size(temp,1)/patchSize(1))
+    
+    idx2 = -overlap(1) + patchSize(1)*(i-1)+1; idx2 = idx2:idx2+patchSize(1)-1;
+    idx = idx2-2*overlap(1)*(i-1);
+    goodDex1 = idx>0 & idx<=sz(1); idx = idx(goodDex1);
+    goodDex2 = idx2>0 & idx2<=size(temp,1); idx2 = idx2(goodDex2);
+    A(idx,idx2) = A(idx,idx2) + toAdd(goodDex1,goodDex2);
+end
+A = A./sum(A,2);
+
+% Initialize the row transformation matrix
+B = zeros(sz(2),size(temp,2),dType);
+for i = 1:ceil(size(temp,2)/patchSize(2))
+    idx2 = -overlap(2) + patchSize(2)*(i-1)+1; idx2 = idx2:idx2+patchSize(2)-1;
+    idx = idx2-2*overlap(2)*(i-1);
+    goodDex1 = idx>0 & idx<=sz(2); idx = idx(goodDex1);
+    goodDex2 = idx2>0 & idx2<=size(temp,2); idx2 = idx2(goodDex2);
+    B(idx,idx2) = B(idx,idx2)  + toAdd(goodDex1,goodDex2);
+end
+B = B./sum(B,2);
+
+
+X = (A * temp * B');
+% figure; imagesc(vm(cat(3,XC,X)))
+% figure;moviesc(vm(gather(cat(3,X,(A * temp * B')./denom))));
+% figure;moviesc(vm(gather(cat(3,B,A * ones(size(temp)) * B2'))));axis image;colormap gray;
+
+end
+%
--- cell2mat_ov_sum.m
+++ cell2mat_ov_sum.m
@@ -21,25 +21,25 @@ else
 end
 patchSize = size(I{round(size(I,1)/2),round(size(I,2)/2)});
 sameSize = cellfun(@(X) all(size(X)==patchSize),I);
-goodCols = any(sameSize~=0,1);
-goodRows = any(sameSize~=0,2);
-temp = cat(3,I{goodRows,goodCols});
-temp = reshape(temp,patchSize(1),patchSize(2),sum(goodRows),sum(goodCols));
+nonBorderCols = any(sameSize~=0,1);
+nonBorderRows = any(sameSize~=0,2);
+temp = cat(3,I{nonBorderRows,nonBorderCols});
+temp = reshape(temp,patchSize(1),patchSize(2),sum(nonBorderRows),sum(nonBorderCols));
 temp = permute(temp,[1,3,2,4]);
-temp = reshape(temp,patchSize(1)*sum(goodRows),patchSize(2)*sum(goodCols));
+temp = reshape(temp,patchSize(1)*sum(nonBorderRows),patchSize(2)*sum(nonBorderCols));
 
-idxBadRows = find(~goodRows);
-preBadRows = idxBadRows(idxBadRows<find(goodRows,1));
-postBadRows = idxBadRows(idxBadRows>find(goodRows,1));
-for i = 1:sum(preBadRows>0)
-    temp = cat(1,cat(2,I{preBadRows(i),goodCols}),temp);
+idxBorderRows = find(~nonBorderRows);
+preBorderRows = idxBorderRows(idxBorderRows<find(nonBorderRows,1));
+postBorderRows = idxBorderRows(idxBorderRows>find(nonBorderRows,1));
+for i = 1:sum(preBorderRows>0)
+    temp = cat(1,cat(2,I{preBorderRows(i),nonBorderCols}),temp);
 end
-for i = 1:numel(postBadRows)
-    temp = cat(1,temp,cat(2,I{postBadRows(i),goodCols}));
+for i = 1:numel(postBorderRows)
+    temp = cat(1,temp,cat(2,I{postBorderRows(i),nonBorderCols}));
 end
-idxBadCols = find(~goodCols);
-preBadCols = idxBadCols(idxBadCols<find(goodCols,1));
-postBadCols = idxBadCols(idxBadCols>find(goodCols==0,1));
+idxBadCols = find(~nonBorderCols);
+preBadCols = idxBadCols(idxBadCols<find(nonBorderCols,1));
+postBadCols = idxBadCols(idxBadCols>find(nonBorderCols==0,1));
 for i = 1:numel(preBadCols)
     temp = cat(2,cat(1,I{:,preBadCols(i)}),temp);
 end
@@ -48,17 +48,14 @@ for i = 1:numel(postBadCols)
 end
 % figure;imagesc(temp);axis image;colormap gray;
 
-
-
-
-
 % Initialize the row transformation matrix
 A = zeros(sz(1),size(temp,1),dType);
 
+toAdd = (eye(patchSize(1),dType).*[1:overlap(1) (overlap(1)*ones(1,patchSize(1)-2*overlap(1),dType)) overlap(1):-1:1]);
 for i = 1:ceil(size(temp,1)/patchSize(1))
-    idx2 = patchSize(1)*(i-1)+1; idx2 = idx2:idx2+patchSize(1)-1;
+    
+    idx2 = -overlap(1) + patchSize(1)*(i-1)+1; idx2 = idx2:idx2+patchSize(1)-1;
     idx = idx2-2*overlap(1)*(i-1);
-    toAdd = (eye(patchSize(1),dType).*[1:overlap(1) (overlap(1)*ones(1,patchSize(1)-2*overlap(1),dType)) overlap(1):-1:1]);
     goodDex1 = idx>0 & idx<=sz(1); idx = idx(goodDex1);
     goodDex2 = idx2>0 & idx2<=size(temp,1); idx2 = idx2(goodDex2);
     A(idx,idx2) = A(idx,idx2) + toAdd(goodDex1,goodDex2);
@@ -68,9 +65,8 @@ A = A./sum(A,2);
 % Initialize the row transformation matrix
 B = zeros(sz(2),size(temp,2),dType);
 for i = 1:ceil(size(temp,2)/patchSize(2))
-    idx2 = patchSize(2)*(i-1)+1; idx2 = idx2:idx2+patchSize(2)-1;
+    idx2 = -overlap(2) + patchSize(2)*(i-1)+1; idx2 = idx2:idx2+patchSize(2)-1;
     idx = idx2-2*overlap(2)*(i-1);
-    toAdd = (eye(patchSize(2),dType).*[1:overlap(2) (overlap(2)*ones(1,patchSize(2)-2*overlap(2),dType)) overlap(2):-1:1]);
     goodDex1 = idx>0 & idx<=sz(2); idx = idx(goodDex1);
     goodDex2 = idx2>0 & idx2<=size(temp,2); idx2 = idx2(goodDex2);
     B(idx,idx2) = B(idx,idx2)  + toAdd(goodDex1,goodDex2);
@@ -79,45 +75,9 @@ B = B./sum(B,2);
 
 
 X = (A * temp * B');
+% figure; imagesc(vm(cat(3,XC,X)))
 % figure;moviesc(vm(gather(cat(3,X,(A * temp * B')./denom))));
 % figure;moviesc(vm(gather(cat(3,B,A * ones(size(temp)) * B2'))));axis image;colormap gray;
 
-end
-
-function X = oldVersion(I,xx_s,xx_f,yy_s,yy_f,zz_s,zz_f,overlap,sz,Bs)
-tic
-if isa(I{1},'gpuArray')
-    dType = 'gpuArray';
-else
-    dType = 'single';
-end
-if nargin < 10 || isempty(Bs)
-    Bs = cellfun(@(x) ones(size(x),dType), I,'un',0);
-end
-X = zeros([sz,size(I{1,1},length(sz)+1)],dType);
-B = zeros(size(X),dType);
-if length(sz) == 2; sz(3) = 1; end
-
-for i = 1:length(xx_f)
-    for j = 1:length(yy_f)
-        for k = 1:length(zz_f)
-            extended_grid = [max(xx_s(i)-overlap(1),1),min(xx_f(i)+overlap(1),sz(1)),max(yy_s(j)-overlap(2),1),min(yy_f(j)+overlap(2),sz(2)),max(zz_s(k)-overlap(3),1),min(zz_f(k)+overlap(3),sz(3))];
-            %W = construct_weights([xx_s(i),xx_f(i),yy_s(j),yy_f(j),zz_s(k),zz_f(k)],extended_grid)';   
-            
-            Xtemp = zeros(size(I{i,j,k}),dType);
-            Btemp = Xtemp;
-            ind = ~isnan(I{i,j,k});
-            Xtemp(ind) = Bs{i,j,k}(ind).*I{i,j,k}(ind);
-            Btemp(ind) = Bs{i,j,k}(ind);
-            %X(extended_grid(1):extended_grid(2),extended_grid(3):extended_grid(4),extended_grid(5):extended_grid(6)) = X(extended_grid(1):extended_grid(2),extended_grid(3):extended_grid(4),extended_grid(5):extended_grid(6)) + Bs{i,j,k}.*I{i,j,k}; 
-            %B(extended_grid(1):extended_grid(2),extended_grid(3):extended_grid(4),extended_grid(5):extended_grid(6)) = B(extended_grid(1):extended_grid(2),extended_grid(3):extended_grid(4),extended_grid(5):extended_grid(6)) + Bs{i,j,k}.*(I{i,j,k}~=0);
-            X(extended_grid(1):extended_grid(2),extended_grid(3):extended_grid(4),extended_grid(5):extended_grid(6)) = X(extended_grid(1):extended_grid(2),extended_grid(3):extended_grid(4),extended_grid(5):extended_grid(6)) + Xtemp; 
-            B(extended_grid(1):extended_grid(2),extended_grid(3):extended_grid(4),extended_grid(5):extended_grid(6)) = B(extended_grid(1):extended_grid(2),extended_grid(3):extended_grid(4),extended_grid(5):extended_grid(6)) + Btemp;            
-        end
-    end
-end
-
-X = X./B;
-toc
 end
 %
